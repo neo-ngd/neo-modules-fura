8d1b950a3f19caf4dce0e2255806d515836ca6c0
vikkkko <82604458@qq.com>
2021-08-23 11:27:13 +0800

    增加opcode2pushnumber的类型判断

--- UnitFuraTest/FuraTest.cs
+++ UnitFuraTest/FuraTest.cs
@@ -25,7 +25,7 @@ namespace UnitFuraTest
         [TestMethod]
         public void TestScript2Executions()
         {
-            var base64String = "CwwUaUQlwX8eu3xl3jAmyDHrTEnW174SwB8MBHZvdGUMFPVj6kC8KD1NDgXEjqMFs/Kgc0DvQWJ9W1I=";
+            var base64String = "EBAQExETDAAMAAwOUmVzcG9uc2liaWxpdHkMDURldGVybWluYXRpb24MBkxlYWRlcgwETWFsZQwKQ3J5cHRvbmF1dAyUVGhlIGZpcnN0IENyeXB0b25hdXQgdG8gbGFuZCBvbiB0aGUgbW9vbi4gLjAwMSBpcyB0aGUgcGlvbmVlciBvZiB0aGUgQ3J5cHRvbmF1dHMsIHdpdGggdGhlIHJlbGlhYmlsaXR5IGFuZCBkcml2ZSB0byBtYWludGFpbiBoaXMgaGVhZCBob25jaG8gc3RhdHVzLgxOaHR0cHM6Ly9taW50aW5nLXR0bS5zMy5hbWF6b25hd3MuY29tL05FVytNT09OKy0rQ1JZUFRPTkFVVFMvQ3J5cHRvbmF1dC4wMDEuc3ZnDANHSU4MDVNvdXRoIEFtZXJpY2EMCE5ldyBNb29uEQwETW9vbgwPQ3J5cHRvbmF1dCAuMDAxDARDMDAxDAlDaGFyYWN0ZXIAWgwUscULnROUsRBiD1tctANzvWBf6+MAGcAfDA1taW50Q2hhcmFjdGVyDBR8+ztrd+pH3tZmQRtewFdPJvVU5kFifVtS";
             //var base64String = "DCEC13y+vWO9KxAxFwg0SF0rjAJoq/n2N89uNwqrDwi+WHsMFIU5Il4pKR6Kf5xyOLaNS67/1PekEsAfDAR2b3RlDBT1Y+pAvCg9TQ4FxI6jBbPyoHNA70FifVtS";
             var script = Convert.FromBase64String(base64String);
             var scCalls = Neo.Plugins.VM.Helper.Script2ScCallModels(script,UInt256.Zero, UInt160.Zero);
