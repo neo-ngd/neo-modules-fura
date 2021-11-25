2652189a19959714d853feddfead0cd8037cc9f9
vikkkko <82604458@qq.com>
2021-11-25 16:42:06 +0800

    add

---
+++ Fura/Notification/NotificationMgr.Destroy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Neo.Network.P2P.Payloads;
+using Neo.Persistence;
+using Neo.Plugins.Cache;
+using Neo.Plugins.Models;
+using Neo.SmartContract.Native;
+
+namespace Neo.Plugins.Notification
+{
+    public partial class NotificationMgr
+    {
+        private bool ExecuteDestroyNotification(NotificationModel notificationModel, NeoSystem system, Block block, DataCache snapshot)
+        {
+            if (notificationModel.ContractHash == NativeContract.ContractManagement.Hash)
+            {
+                UInt160 contractHash = null;
+                bool succ = UInt160.TryParse(Convert.FromBase64String(notificationModel.State.Values[0].Value).Reverse().ToArray().ToHexString(), out contractHash);
+                if (!succ) return false;
+                DBCache.Ins.cacheContract.AddNeedUpdate(contractHash, block.Timestamp, notificationModel.Txid, true);
+            }
+            return true;
+        }
+    }
+}
+
