a414b92e36542cb6ffbd31a28814294377b66ace
vikkkko <82604458@qq.com>
2022-01-12 14:32:28 +0800

    补上个提交遗漏

---
+++ Fura/Notification/NotificationMgr.Market.RemoveAsset.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Neo.Network.P2P.Payloads;
+using Neo.Persistence;
+using Neo.Plugins.Cache;
+using Neo.Plugins.Models;
+using Neo.SmartContract.Native;
+using System.Numerics;
+using Neo.IO.Json;
+
+namespace Neo.Plugins.Notification
+{
+    public partial class NotificationMgr
+    {
+        private bool ExecuteRemoveAssetNotification(NotificationModel notificationModel, NeoSystem system, Block block, DataCache snapshot)
+        {
+            ContractModel contractModel = DBCache.Ins.cacheContract.Get(notificationModel.ContractHash);
+            if (Settings.Default.MarketContractIds.Contains(contractModel._ID))
+            {
+                UInt160 asset = null;
+                bool succ = true;
+                //asset
+                if (notificationModel.State.Values[0].Value is not null)
+                {
+                    succ = succ && UInt160.TryParse(Convert.FromBase64String(notificationModel.State.Values[0].Value).Reverse().ToArray().ToHexString(), out asset);
+                }
+
+                DBCache.Ins.cacheMatketNotification.Add(notificationModel.Txid, notificationModel.BlockHash, notificationModel.ContractHash, 0, null, asset, "", "RemoveAsset", "{}", notificationModel.Timestamp);
+            }
+            return true;
+        }
+    }
+}
+
