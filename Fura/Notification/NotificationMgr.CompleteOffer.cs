e9f869c63db91439f359b40ae62baa05f44f3c77
yinwei <82604458@qq.com>
2022-06-24 20:16:06 +0800

    completeOffer通知增加售卖人的信息

--- Fura/Notification/NotificationMgr.CompleteOffer.cs
+++ Fura/Notification/NotificationMgr.CompleteOffer.cs
@@ -19,6 +19,7 @@ namespace Neo.Plugins.Notification
             {
                 //(nonce, 用户 ,求购使用的nep17资产，nep17数额，求购的nft的hash，求购的nfttokenid，求购截止日期)
                 BigInteger nonce = 0;
+                UInt160 sender = null;
                 UInt160 user = null;
                 UInt160 offerAsset = null;
                 BigInteger offerAmount = 0;
@@ -27,37 +28,43 @@ namespace Neo.Plugins.Notification
                 BigInteger endTimestamp = 0;
                 bool succ = true;
                 succ = succ && BigInteger.TryParse(notificationModel.State.Values[0].Value, out nonce);
-                //user
+                //sender
                 if (notificationModel.State.Values[1].Value is not null)
                 {
-                    succ = succ && UInt160.TryParse(Convert.FromBase64String(notificationModel.State.Values[1].Value).Reverse().ToArray().ToHexString(), out user);
+                    succ = succ && UInt160.TryParse(Convert.FromBase64String(notificationModel.State.Values[1].Value).Reverse().ToArray().ToHexString(), out sender);
                 }
-                //offerAsset
+                //user
                 if (notificationModel.State.Values[2].Value is not null)
                 {
-                    succ = succ && UInt160.TryParse(Convert.FromBase64String(notificationModel.State.Values[2].Value).Reverse().ToArray().ToHexString(), out offerAsset);
+                    succ = succ && UInt160.TryParse(Convert.FromBase64String(notificationModel.State.Values[2].Value).Reverse().ToArray().ToHexString(), out user);
+                }
+                //offerAsset
+                if (notificationModel.State.Values[3].Value is not null)
+                {
+                    succ = succ && UInt160.TryParse(Convert.FromBase64String(notificationModel.State.Values[3].Value).Reverse().ToArray().ToHexString(), out offerAsset);
                 }
                 //offerAmount
-                succ = succ && BigInteger.TryParse(notificationModel.State.Values[3].Value, out offerAmount);
+                succ = succ && BigInteger.TryParse(notificationModel.State.Values[4].Value, out offerAmount);
                 //asset
-                if (notificationModel.State.Values[4].Value is not null)
+                if (notificationModel.State.Values[5].Value is not null)
                 {
-                    succ = succ && UInt160.TryParse(Convert.FromBase64String(notificationModel.State.Values[4].Value).Reverse().ToArray().ToHexString(), out asset);
+                    succ = succ && UInt160.TryParse(Convert.FromBase64String(notificationModel.State.Values[5].Value).Reverse().ToArray().ToHexString(), out asset);
                 }
                 //tokenid
-                if (notificationModel.State.Values[5].Type == "Integer")  //需要转换一下
+                if (notificationModel.State.Values[6].Type == "Integer")  //需要转换一下
                 {
-                    tokenId = Convert.ToBase64String(BigInteger.Parse(notificationModel.State.Values[5].Value).ToByteArray());
+                    tokenId = Convert.ToBase64String(BigInteger.Parse(notificationModel.State.Values[6].Value).ToByteArray());
                 }
                 else
                 {
-                    tokenId = notificationModel.State.Values[5].Value;
+                    tokenId = notificationModel.State.Values[6].Value;
                 }
                 //endtimestamp
-                succ = succ && BigInteger.TryParse(notificationModel.State.Values[6].Value, out endTimestamp);
+                succ = succ && BigInteger.TryParse(notificationModel.State.Values[7].Value, out endTimestamp);
 
 
                 JObject json = new JObject();
+                json["sender"] = offerAsset?.ToString();
                 json["offerAsset"] = offerAsset?.ToString();
                 json["offerAmount"] = offerAmount.ToString();
                 json["deadline"] = endTimestamp.ToString();
